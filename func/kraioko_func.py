ff745bd61f1c1b484de6e7051591d9d60a519bd9
grigory <bga20100@gmail.com>
2024-07-14 15:29:37 +0500

    Fix Kraioko Cookies Update

--- func/kraioko_func.py
+++ func/kraioko_func.py
@@ -9,18 +9,22 @@ KRAIOKO_RES_SCRIPT_URL = 'https://kraioko.perm.ru/utils/results/loadstudentresul
 # Основная функция проверки результатов по паспортным данным
 def kraioko_check(passp):
     try:
-        s = requests.get(KRAIOKO_URL, timeout=7).text
-        index_1 = s.find('rhash')
-        rhash = s[index_1+7:index_1+7+44]
-
-        params = {'ds': passp[:4], 'dn':passp[4:], 'rhash': rhash}
+        s = requests.Session()
+        r = s.get(KRAIOKO_URL, timeout=7)        
+        params = s.cookies.get_dict()
+        params |= {'ds': passp[:4], 'dn':passp[4:]}
         r = requests.get(KRAIOKO_RES_SCRIPT_URL, params=params, timeout=7)
         if r.status_code != 200:
             return 400
         result = r.content
     
-        soup = BeautifulSoup(result, 'lxml')
+        soup = BeautifulSoup(result, 'xml', from_encoding='utf-8')
+        k = soup.find('tabletext')
+        k = str(k).replace('&lt;', '<').replace('&gt;', '>')
+
+        soup = BeautifulSoup(k, 'lxml')
         data = []
+        
         table = soup.find('table')
         if table is None:
             return False
@@ -35,6 +39,7 @@ def kraioko_check(passp):
         return data
     except:
         return 400
+
 
 # Функция распаковки результатов из списка
 def unpack_results(data) -> str:    
